576fbc2b65d081923d0c30f63dc129bb0a573420
sanprasirt <sanprasirt@pccth.com>
2023-11-18 08:51:28 +0700

    Update opentracing with zipkin

--- src/main/java/vn/cloud/helloservice/HelloServiceApplication.java
+++ src/main/java/vn/cloud/helloservice/HelloServiceApplication.java
@@ -1,11 +1,23 @@
 package vn.cloud.helloservice;
 
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class HelloServiceApplication {
+	private static final Log logger = LogFactory.getLog(HelloServiceApplication.class);
 
+
+	@RequestMapping("/")
+	String home() {
+		logger.info("Home() has been called");
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
